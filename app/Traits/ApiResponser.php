42811bf2330e6218a7c1e82a38544529a0e24ea1
Mawojast <inet.beck@gmail.com>
2023-05-08 19:07:27 +0200

    section32 - Caching Results with Laravel Cache System

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -4,6 +4,7 @@ namespace App\Traits;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Validator;
 trait ApiResponser {
 
@@ -29,6 +30,7 @@ trait ApiResponser {
         $collection = $this->sortData($collection, $transformer);
         $collection = $this->paginate($collection);
         $collection = $this->transformData($collection, $transformer);
+        $collection = $this->cacheResponse($collection);
 
         return $this->successResponse($collection, $code);
     }
@@ -102,4 +104,22 @@ trait ApiResponser {
 
         return $paginated;
     }
+
+    protected function cacheResponse($data){
+
+        $url = request()->url();
+        $queryParams = request()->query();
+
+        ksort($queryParams);
+
+        $queryString = http_build_query($queryParams);
+
+        $fullUrl = "{$url}?{$queryString}";
+        //dd($fullUrl);
+        return Cache::remember($fullUrl, 30, function() use($data){
+            return $data;
+        });
+    }
+
+
 }
